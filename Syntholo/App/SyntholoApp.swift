958ce5b9f8e1cccae3768ef8c7e20b32123c27e2
Karim Aly <karim.aly@su.suffolk.edu>
2026-08-25 23:09:48 -0400

    fix: hide bootstrap host copy

--- Syntholo/App/SyntholoApp.swift
+++ Syntholo/App/SyntholoApp.swift
@@ -4,7 +4,9 @@ import SwiftUI
 struct SyntholoApp: App {
     var body: some Scene {
         WindowGroup {
-            Text("Syntholo")
+            Color.clear
+                .ignoresSafeArea()
+                .accessibilityHidden(true)
         }
     }
 }
